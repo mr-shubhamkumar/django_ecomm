583f23b8dbe78cbebbfe97ab0a85f91e4017a2df
Shubham Kumar <mr.dev.shubham@gmail.com>
2023-12-08 12:33:43 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.views import View
+from .models import  Customer, Product, Cart, OrderPlaced
+# def home(request):
+#  return render(request, 'app/home.html')
+class ProductView(View):
+    def get(self, request):
+        topwears = Product.objects.filter(category='TW')
+        bottompwears = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
+        laptop = Product.objects.filter(category='L')
+        return render(request,'app/home.html',{'topwears':topwears,'bottompwears':bottompwears,'mobiles':mobiles,'laptop':laptop,
+        })
+
+
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        # print(product)
+        return render(request, 'app/productdetail.html',{'product':product})
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def profile(request):
+ return render(request, 'app/profile.html')
+
+def address(request):
+ return render(request, 'app/address.html')
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+def change_password(request):
+ return render(request, 'app/changepassword.html')
+
+def mobile(request, data=None):
+    if data == None:
+        mobiles = Product.objects.filter(category='M')
+    elif data == 'Google' or 'Infinix' or 'Nothing' or 'vivo' or 'MOTOROLA' or 'POCO':
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+    return render(request, 'app/mobile.html', {'mobiles':mobiles})
+
+def login(request):
+ return render(request, 'app/login.html')
+
+def customerregistration(request):
+ return render(request, 'app/customerregistration.html')
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
